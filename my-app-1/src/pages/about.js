7de0638ee288a5663c7d6442c25f03c0aee19d5f
DewitaA12 <xsclthzn@gmail.com>
2026-02-25 10:44:41 +0700

    create pages route

---
+++ my-app-1/src/pages/about.js
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Halaman About</h1>
+      <p><strong>Nama Mahasiswa:</strong> Dewita</p>
+      <p><strong>NIM:</strong> 123456789</p>
+      <p><strong>Program Studi:</strong> Teknik Informatika</p>
+    </div>
+  );
+}
