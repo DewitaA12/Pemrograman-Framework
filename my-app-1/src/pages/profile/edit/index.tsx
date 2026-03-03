85bca813a71c43bf70bc0162edbebf06e866b13e
DewitaA12 <xsclthzn@gmail.com>
2026-03-03 21:33:44 +0700

    task_1 routing

---
+++ my-app-1/src/pages/profile/edit/index.tsx
@@ -0,0 +1,7 @@
+export default function Edit() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Ini Halaman Edit</h1>
+    </div>
+  );
+}
---
+++ my-app-1/src/pages/profile/index.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Ini Halaman Profile</h1>
+    </div>
+  );
+}
