a3c84e644453721895a152c782710d8af2b65151
DewitaA12 <xsclthzn@gmail.com>
2026-03-03 21:03:41 +0700

    imp navbar all pages

--- my-app-1/src/pages/_app.tsx
+++ my-app-1/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '@/components/layouts/Navbar'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div><Navbar /><Component {...pageProps} /></div>
 }
