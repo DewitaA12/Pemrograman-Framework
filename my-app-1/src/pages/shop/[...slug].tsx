d331238ac240e9a4e7baa044288cdf1033f37d40
DewitaA12 <xsclthzn@gmail.com>
2026-03-04 08:57:19 +0700

    imp scaleable catch-all route

--- my-app-1/src/pages/shop/[...slug].tsx
+++ my-app-1/src/pages/shop/[...slug].tsx
@@ -9,7 +9,8 @@ const { query } = useRouter();
     <div>
       <h1>Halaman Toko</h1>
       <p>
-        Toko: {query.slug && query.slug[0] + "-" + query.slug[1]}
+        {/* Toko: {query.slug && query.slug[0] + "-" + query.slug[1]} */}
+        Toko: {Array.isArray(query.slug) ? query.slug.join(".") : query.slug}
         </p>
     </div>
   );
