b8b1903089754a70a3935f7d0d98c7f746743831
DewitaA12 <xsclthzn@gmail.com>
2026-03-07 23:09:14 +0700

    task_1 modified register pages

---
+++ my-app-1/src/views/auth/register/index.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from "./register.module.css";
+
+const HalamanRegister = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Halaman Register</h1>
+
+      <Link href="/auth/login" className={styles.link}>
+        Ke Halaman Login
+      </Link>
+    </div>
+  );
+};
+
+export default HalamanRegister;
