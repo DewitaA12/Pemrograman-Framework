6b51dd614ea3b3c19a16c652fcdc0204bfbe39b4
DewitaA12 <xsclthzn@gmail.com>
2026-04-09 16:10:51 +0700

    add basic middleware structure

--- my-app-1/src/pages/produk/index.tsx
+++ my-app-1/src/pages/produk/index.tsx
@@ -5,26 +5,17 @@ import useSWR from "swr";
 import fetcher from "../../utils/swr/fetcher";
 
 const kategori = () => {
-  // const [isLogin, setIsLogin] = useState(false);
-  // const { push } = useRouter();
+  const [isLogin, setIsLogin] = useState(true);
+  const { push } = useRouter();
   const [products, setProducts] = useState([]);
   // console.log("products:", products);
 
-  // useEffect(() => {
-  //   if (!isLogin) {
-  //     push("/auth/login");
-  //   }
-  // },[]);
-
   const { data, error, isLoading } = useSWR("/api/produk", fetcher);
-  //cek apakah data, error, dan isLoading sudah benar
-  // console.log("data:", data);
-  // console.log("error:", error);
-  // console.log("isLoading:", isLoading);
+  //cek apakah data, error, dan isLoading sudah benar...
 
   return (
     <div>
-      <TampilanProduk products={isLoading ? [] : data?.data ?? []} />
+      <TampilanProduk products={isLoading ? [] : data.data} />
     </div>
   );
 };
