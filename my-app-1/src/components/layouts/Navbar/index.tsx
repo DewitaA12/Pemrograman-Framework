858cf6dd524e9d6c8b942a5d1c242821c701ad4f
DewitaA12 <xsclthzn@gmail.com>
2026-04-13 16:41:08 +0700

    add sign out buttons

--- my-app-1/src/components/layouts/Navbar/index.tsx
+++ my-app-1/src/components/layouts/Navbar/index.tsx
@@ -2,12 +2,23 @@ import syles from './navbar.module.css';
 import { signIn, signOut, useSession } from "next-auth/react"
 
 const Navbar = () => {
+  const {data}:any = useSession()
+  //const { data: session } = useSession()
+  // console.log("session", session)
   return (
     <div className={syles.navbar}>
       <div className="big">
         Navbar
       </div>
-      <button onClick={() => signIn()}>Sign In</button>
+      {data ? (
+        <>
+          <button onClick={() => signOut()}>Sign Out</button>
+        </>
+      ) : (
+        <>
+          <button onClick={() => signIn()}>Sign In</button>
+        </>
+      )}
     </div>
   );
 };
