5aee3338b3b4b9e1a08e4087d26b9e25ffe984cb
DewitaA12 <xsclthzn@gmail.com>
2026-04-13 17:31:37 +0700

    add profile page and middleware route protection

--- my-app-1/src/middleware.ts
+++ my-app-1/src/middleware.ts
@@ -1,19 +1,16 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import type { NextFetchEvent, NextRequest } from 'next/server'
+import withAuth from './middleware/withAuth'
 
-export function middleware(request: NextRequest) {
-  const isLogin = false;
-
-  if (isLogin) {
-    return NextResponse.next();
-  } else {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
-
-  //return NextResponse.redirect(new URL("/", request.url));
-  //return NextResponse.next();
+export function middleware(req: NextRequest, event: NextFetchEvent) {
+  return withAuth(
+    (req) => {
+      return NextResponse.next();
+    },
+    ["/profile"]
+  )(req, event);
 }
 
 export const config = {
-  matcher: ["/produk", "/about"],
+  matcher: ["/profile"],
 };
