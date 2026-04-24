61445a587b98bac4b5176facf65063dc17e0ca46
DewitaA12 <xsclthzn@gmail.com>
2026-04-24 08:08:21 +0700

    imp script optimization

--- my-app-1/src/components/layouts/Navbar/index.tsx
+++ my-app-1/src/components/layouts/Navbar/index.tsx
@@ -1,13 +1,22 @@
+import Script from "next/script";
 import styles from "./navbar.module.css";
 import { signIn, signOut, useSession } from "next-auth/react";
-import Image from "next/image"; // ✅ Ganti <img> biasa dengan next/image
+import Image from "next/image";
 
 const Navbar = () => {
   const { data }: any = useSession();
 
   return (
     <div className={styles.navbar}>
-      <div className={styles.navbar__brand}>MyApp</div>
+      {/* Title kosong (akan diisi oleh script) */}
+      <div className={styles.navbar__brand} id="title"></div>
+
+      {/* Script Optimization */}
+      <Script id="title-script" strategy="lazyOnload">
+        {`
+          document.getElementById('title').innerHTML = 'MyApp';
+        `}
+      </Script>
 
       <div className={styles.navbar__right}>
         {data ? (
@@ -15,7 +24,6 @@ const Navbar = () => {
             <div className={styles.navbar__user}>
               Welcome, {data.user?.fullname}
 
-              {/* ✅ Tugas Mandiri No. 5 — next/image menggantikan <img> biasa */}
               {data.user?.image && (
                 <Image
                   src={data.user.image}
@@ -23,7 +31,6 @@ const Navbar = () => {
                   width={42}
                   height={42}
                   className={styles.navbar__user__image}
-                  // priority agar gambar dimuat lebih cepat di atas fold
                   priority
                 />
               )}
@@ -37,14 +44,12 @@ const Navbar = () => {
             </button>
           </>
         ) : (
-          <>
-            <button
-              className={`${styles.navbar__button} ${styles["navbar__button--primary"]}`}
-              onClick={() => signIn()}
-            >
-              Sign In
-            </button>
-          </>
+          <button
+            className={`${styles.navbar__button} ${styles["navbar__button--primary"]}`}
+            onClick={() => signIn()}
+          >
+            Sign In
+          </button>
         )}
       </div>
     </div>
