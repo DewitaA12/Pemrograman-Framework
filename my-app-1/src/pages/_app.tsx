6d64579e02f013a29b275916f27582a2425ca786
DewitaA12 <xsclthzn@gmail.com>
2026-04-24 08:42:30 +0700

    add script google analytics

--- my-app-1/src/pages/_app.tsx
+++ my-app-1/src/pages/_app.tsx
@@ -2,10 +2,32 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import AppShell from "@/components/layouts/Appshell";
 import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <SessionProvider session={session}>
+      {/* Google Analytics Script */}
+      
+      {/* Load GA library */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+        strategy="afterInteractive"
+      />
+
+      {/* Init GA */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XXXXXXX');
+        `}
+      </Script>
+
       <AppShell>
         <Component {...pageProps} />
       </AppShell>
